878180e9a02512f440ec2f0c8c4ac1b75909e12d
Dan <dannyagah13@gmail.com>
2023-06-04 18:58:18 +0300

    make chnages

---
+++ htllProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for htllProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'htllProject.settings')
+
+application = get_asgi_application()
